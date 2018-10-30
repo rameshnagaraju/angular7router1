a737c9b5fa06800c6e16008723caadee117164df
rameshnagaraju <rameshnagaraju@gmail.com>
2018-10-30 21:45:57 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { BarsComponent } from './bars/bars.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'booksMenu',
+    component: BooksComponent,
+    outlet: 'sidebar'
+  },
+  {
+    path: 'barsMenu',
+    component: BarsComponent,
+    outlet: 'sidebar'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { BarsComponent } from './bars/bars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    BarsComponent
   ],
   imports: [
     BrowserModule,
